2b4f658d0b41919381fa261b05fd6241da0e33b5
TheodoreTsimiklis <70531739+TheodoreTsimiklis@users.noreply.github.com>
2022-12-07 14:59:12 -0500

    Made about us longer added image on main page

--- MVC/app/views/About/index.php
+++ MVC/app/views/About/index.php
@@ -5,7 +5,10 @@
                 </div>
             </div>
             <div class="row d-flex justify-content-center">
-            <div class="col">We are the connection between donors and patients, healthcare professionals and medical researchers. We are nationally responsible for a secure system of life essentials for blood transfusions that's reliable, accessible and sustainable. Every day we work diligently to help save lives, and restore health</div>
+            <div class="col">We are the connection between donors and patients, healthcare professionals and medical researchers. We are nationally responsible for a secure system of life essentials for blood transfusions that's reliable, accessible and sustainable. Every day we work diligently to help save lives, and restore health. Our mission is to efficiently meet the needs of people for quality blood. and by donating your blood, you have the opportunity to help someone in need of it. As such, we are nationally responsible for a secure
+                            system of life essentials for transfusion or transplantation that is reliable, accessible and sustainable. We do the right thing, always. We do what we say we will do.
+                            We share information in an honest, transparent manner. We follow policies, processes and regulations. We give our best every day, in all that we do. We take ownership of our work.
+                            We continuously improve our processes and services.</div>
             </div>
         </div>
     </section>
